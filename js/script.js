2555457a8b28b2ad82665be7ba7fc9b4322a523c
jasonhpiano <jhpiano.ccit@unp.edu.ph>
2023-11-24 16:36:14 +0800

    Added files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Show Password
+const pwd = document.getElementById("pwd");
+const chk = document.getElementById("chk");
+
+chk.onchange = function(e) {
+    pwd.type = chk.checked ? "text" : "password";
+}
